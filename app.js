efecc3950ccc5f0746c3e810b5ff10cdba776647
Osmar <osmardi2711@gmail.com>
2025-07-31 17:19:16 -0300

    hello

---
+++ app.js
@@ -0,0 +1,48 @@
+function olaMundo(){
+    return console.log('olá mundo');
+}
+olaMundo();
+
+
+function nome(){
+    let n;
+    n = prompt('diga seu nome');
+    return console.log(`ola ${n}`);
+}
+nome();
+
+function numero(){
+    let nu;
+    nu = prompt('diga um numero');
+    return console.log(nu*2); 
+}
+numero();
+
+function media(x,y,z){
+    x = Number(prompt('digite o numero 1'));
+    y = Number(prompt('digite o numero 2'));
+    z = Number(prompt('digite o numero 3'));
+    let m = (x + y + z) / 3;
+    console.log(m);
+}
+media();
+
+function maior(d,e){
+    d = Number(prompt('digite o primeiro numero'));
+    e = Number(prompt('digite um segundo numero'));
+    if(d>e)
+        console.log(`o numero ${d} e maio que o numero ${e}`);
+    else
+        console.log(`o numero ${e} e maior que o numero ${d}`);
+}
+maior();
+
+function multi(){
+    let pi;
+    pi = Number(prompt('digite um numero'));
+    let mul = pi * pi;
+    console.log(mul);
+}
+multi();
+
+
